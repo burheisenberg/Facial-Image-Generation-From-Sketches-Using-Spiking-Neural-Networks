c466ed4e243c6175ad379c72465a908d3f55218d
Burhan Karahasan <brhan.k.17@gmail.com>
2024-06-05 02:23:53 +0300

    Add files via upload

---
+++ WeeklyMeetings/Week6/anaconda_activate.bat
@@ -0,0 +1,4 @@
+@echo off
+
+%windir%\System32\cmd.exe "/K" C:\Users\Brhan\anaconda3\Scripts\activate.bat C:\Users\Brhan\anaconda3\envs\norse
+
---
+++ WeeklyMeetings/Week8/anaconda_activate.bat
@@ -0,0 +1,5 @@
+@echo off
+
+%windir%\System32\cmd.exe "/K" C:\Users\Brhan\anaconda3\Scripts\activate.bat C:\Users\Brhan\anaconda3\envs\nengo
+
+
